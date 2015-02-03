8db699b2257b5ce3e66a4e24a9f6f6696afa0bf7
Jeff Kayser <jeff.kayser@gmail.com>
2015-02-03 13:05:32 -0500

    Add common client-side code for search.

---
+++ source/static/js/search.js
@@ -0,0 +1,58 @@
+var SEARCH_INDEX_URL = '/search.json';
+var SEARCH_LANGUAGE = 'english';
+var search_index = null;
+var search_stemmer = null;
+var search_view = null;
+
+function init_search(lang) {
+    if(lang === undefined) {
+        lang = SEARCH_LANGUAGE;
+    }
+    load_index(SEARCH_INDEX_URL);
+    search_stemmer = new Snowball(lang);
+}
+
+function load_index(url) {
+    $.getJSON(url, function(data) { search_index = data; });
+}
+
+function stem(word) {
+    if(search_stemmer == null) {
+        return word;
+    }
+    search_stemmer.setCurrent(word);
+    search_stemmer.stem();
+    return search_stemmer.getCurrent();
+}
+
+function search(text) {
+    if(search_index == null) {
+        return;
+    }
+
+    text = text.trim();
+    if(text == '') {
+        update_search_ui(null);
+    }
+
+    words = text.split(/\s+/);
+    var stemmed = words.map(stem);
+
+    var results = {};
+    var found_indexes = [];
+    for(var index in stemmed) {
+        var word = stemmed[index];
+        if(word in search_index.words) {
+            var hit = search_index.words[word];
+            for(var page in hit) {
+                if(page in found_indexes) {
+                    continue;
+                }
+                found_indexes.push(page);
+                var url = search_index.urls[parseInt(page)];
+                results[url] = search_index.titles[parseInt(page)];
+            }
+        }
+    }
+    update_search_ui(results);
+}
